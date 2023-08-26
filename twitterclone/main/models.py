eab7c8ce3aaa335ad9b885f5f7b3619159a31adc
Nasif Qadri <nsq263@gmail.com>
2023-08-26 00:46:13 -0600

    .

--- twitterclone/main/models.py
+++ twitterclone/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    author = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.title
