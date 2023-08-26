3d2a7826e0c54648bf474d3dad0d6580daa12051
NSQ <nsq263@gmail.com>
2023-08-26 17:55:46 -0600

    commit

--- twitterclone/main/models.py
+++ twitterclone/main/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    body = models.TextField()
+    content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
+    reactions = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     author = models.CharField(max_length=50)
+    sentiment = models.FloatField(default=0.0)
 
     def __str__(self) -> str:
         return self.title
--- twitterclone/main/views.py
+++ twitterclone/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    posts = models.Post.objects.all()
+    data = {
+        "posts": posts
+    }
+    return render(request, "home.html", data)
+
+
+def error_404_view(request, exception):
+    return HttpResponse("<h1>Page not found</h1>", status=404)
