ad3a63686824d1570ba6e036d25956de9df8b707
NSQ <nsq263@gmail.com>
2023-08-26 22:32:16 -0600

    commit

--- twitterclone/main/models.py
+++ twitterclone/main/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     reactions = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    author = models.CharField(max_length=50)
+    author = models.CharField(max_length=50, default="")
     sentiment = models.FloatField(default=0.0)
 
     def __str__(self) -> str:
--- twitterclone/main/views.py
+++ twitterclone/main/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from . import models
+from . import forms
 
 # Create your views here.
-def home(request):
-    posts = models.Post.objects.all()
+def home(request) -> HttpResponseRedirect | HttpResponse:
+    posts = models.Post.objects.all().order_by("-date")
+    if request.method == "POST":
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            post = models.Post()
+            post.title = form.cleaned_data["title"]
+            post.content = form.cleaned_data["content"]
+            post.save()
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponse("Invalid form")
+    else:
+        form = forms.PostForm()
+
     data = {
-        "posts": posts
+        "posts": posts,
+        "postform": form
     }
     return render(request, "home.html", data)
 
